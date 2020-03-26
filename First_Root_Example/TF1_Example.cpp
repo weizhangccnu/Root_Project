54cac1f05e319758dfe62b3dd4d26c02271bc764
weizhangccnu <wzhang@mails.ccnu.edu.cn>
2020-03-25 22:15:11 -0500

    add comments for each command

--- First_Root_Example/TF1_Example.cpp
+++ First_Root_Example/TF1_Example.cpp
@@ -7,40 +7,37 @@
     f1->SetLineWidth(4);                        //set line width 
     f1->SetLineStyle(4);                        //set line style 
 
-    f1->SetTitle("My first Root Plot");     
-    f1->GetXaxis()->SetTitle("Time [ms]");
-    f1->GetXaxis()->CenterTitle();
+    f1->SetTitle("My first Root Plot");         //add title for f1 function
+    f1->GetXaxis()->SetTitle("Time [ms]");      //add x-axis label
+    f1->GetXaxis()->CenterTitle();              //set xlable on center
 
-    f1->GetYaxis()->SetTitle("Value [mV]");
-    f1->GetYaxis()->CenterTitle();
+    f1->GetYaxis()->SetTitle("Value [mV]");     //add y-axis label
+    f1->GetYaxis()->CenterTitle();              //set ylabel on center
 
-    f1->GetYaxis()->SetRangeUser(-1.4, 1.4);
-    f1->Draw();
+    f1->GetYaxis()->SetRangeUser(-1.4, 1.4);    //set y-axis range
+    f1->Draw();                                 //draw f1 function
      
-    f2 = new TF1("f2", "sin(x)", 0, 15);
-    f2->SetLineColor(2); 
-    f2->SetLineWidth(4); 
-    f2->SetLineStyle(8); 
-    f2->SetTitle("My second Root Plot");     
-    f2->GetXaxis()->SetTitle("Time");
-    f2->GetYaxis()->SetTitle("Value");
-    f2->GetYaxis()->SetRangeUser(-1.4, 1.4);
-    //f2->SetLegend(TLegend* 1){fLegend = 1};
-    f2->Draw("SAME"); 
+    f2 = new TF1("f2", "sin(x)", 0, 15);        //new a f2 function
+    f2->SetLineColor(2);                        //set line color
+    f2->SetLineWidth(4);                        //set line width
+    f2->SetLineStyle(8);                        //set line style
+    f2->SetTitle("My second Root Plot");        //set f2 function title
+    f2->GetXaxis()->SetTitle("Time");           //set x-axis label
+    f2->GetYaxis()->SetTitle("Value");          //set y-axis label
+    f2->GetYaxis()->SetRangeUser(-1.4, 1.4);    //set y-axis range
+    f2->Draw("SAME");                           //draw f1 and f2 in the same canvas
     
-    auto legend = new TLegend(0.78, 0.8, 0.9, 0.9);
-    legend->AddEntry(f1, "sin(x)/x");
-    legend->AddEntry(f2, "sin(x)");
-    legend->Draw("SAME");
+    auto legend = new TLegend(0.78, 0.8, 0.9, 0.9);         //add legend
+    legend->AddEntry(f1, "sin(x)/x");                       //add f1 legend
+    legend->AddEntry(f2, "sin(x)");                         //add f2 legend
+    legend->Draw();                                         //draw legend
 
-    c->Update();
+    c->Update();                                //update canvas
 
-    //gSystem->ProcessEvents();
+    TImage *img = TImage::Create();             //define a TImage pointer
 
-    TImage *img = TImage::Create();
+    img->FromPad(c);                
+    img->WriteImage("Sin_Waveform.png");        //save canvas to png image
 
-    img->FromPad(c);
-    img->WriteImage("Sin_Waveform.png");
-
-    c->SaveAs("Sin_Waveform.pdf");
+    c->SaveAs("Sin_Waveform.pdf");              //save canvas to pdf 
 }
